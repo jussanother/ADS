c88f9d692653d45ee29735032deef7ec7ecd7e93
jussanother <138112763+jussanother@users.noreply.github.com>
2023-12-02 08:14:53 +0530

    Add files via upload

---
+++ avllab.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int height(struct Node *node) {
+    if (node == NULL)
+        return 0;
+    return node->height;
+}
+
+int getBalance(struct Node *node) {
+    if (node == NULL)
+        return 0;
+    return height(node->left) - height(node->right);
+}
+
+struct Node *newNode(int data) {
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1;
+    return node;
+}
+
+struct Node *rightRotate(struct Node *y) {
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = max(height(y->left), height(y->right)) + 1;
+    x->height = max(height(x->left), height(x->right)) + 1;
+
+    return x;
+}
+
+struct Node *leftRotate(struct Node *x) {
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+
+    return y;
+}
+
+struct Node *insert(struct Node *node, int data) {
+    if (node == NULL)
+        return newNode(data);
+
+    if (data < node->data)
+        node->left = insert(node->left, data);
+    else if (data > node->data)
+        node->right = insert(node->right, data);
+    else
+        return node; // Duplicate data not allowed
+
+    node->height = 1 + max(height(node->left), height(node->right));
+
+    int balance = getBalance(node);
+
+    // Left Heavy
+    if (balance > 1) {
+        if (data < node->left->data) {
+            return rightRotate(node);
+        } else {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+    }
+
+    // Right Heavy
+    if (balance < -1) {
+        if (data > node->right->data) {
+            return leftRotate(node);
+        } else {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+    }
+
+    return node;
+}
+
+struct Node *minValueNode(struct Node *node) {
+    struct Node *current = node;
+    while (current->left != NULL)
+        current = current->left;
+    return current;
+}
+
+struct Node *deleteNode(struct Node *root, int data) {
+    if (root == NULL)
+        return root;
+
+    if (data < root->data)
+        root->left = deleteNode(root->left, data);
+    else if (data > root->data)
+        root->right = deleteNode(root->right, data);
+    else {
+        if ((root->left == NULL) || (root->right == NULL)) {
+            struct Node *temp = root->left ? root->left : root->right;
+
+            if (temp == NULL) {
+                temp = root;
+                root = NULL;
+            } else
+                *root = *temp;
+
+            free(temp);
+        } else {
+            struct Node *temp = minValueNode(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+
+    if (root == NULL)
+        return root;
+
+    root->height = 1 + max(height(root->left), height(root->right));
+
+    int balance = getBalance(root);
+
+    if (balance > 1) {
+        if (getBalance(root->left) >= 0)
+            return rightRotate(root);
+        else {
+            root->left = leftRotate(root->left);
+            return rightRotate(root);
+        }
+    }
+
+    if (balance < -1) {
+        if (getBalance(root->right) <= 0)
+            return leftRotate(root);
+        else {
+            root->right = rightRotate(root->right);
+            return leftRotate(root);
+        }
+    }
+
+    return root;
+}
+
+void preOrder(struct Node *root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int main() {
+    struct Node *root = NULL;
+    int value;
+
+    printf("Enter values to insert into the AVL tree. Enter -1 to stop:\n");
+
+    while (1) {
+        printf("Enter a value: ");
+        scanf("%d", &value);
+
+        if (value == -1) {
+            break;  // Terminate the loop when -1 is entered
+        }
+
+        root = insert(root, value);
+    }
+
+    printf("\nPreorder traversal of AVL tree:\n");
+    preOrder(root);
+
+    printf("\n\nEnter a value to delete from the AVL tree: ");
+    scanf("%d", &value);
+    root = deleteNode(root, value);
+
+    printf("\nPreorder traversal after deletion:\n");
+    preOrder(root);
+
+    return 0;
+}
